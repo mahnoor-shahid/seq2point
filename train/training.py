053590011c613b490736db2b5cb237b1d1a7cbd7
Mahnoor Shahid <mahnoor_shahid_shakir@outlook.com>
2022-11-15 22:52:03 +0100

    save model and loss set to mean

--- train/training.py
+++ train/training.py
@@ -39,10 +39,10 @@ def network_training(model, criterion, optimizer, train_loader, validation_loade
                 loss.backward()
                 optimizer.step()
 
-                if (batch_idx+1) % 10 == 0:
-                    print(f"Epoch : [{epoch+1}/{TRAINING_CONFIG['NUM_EPOCHS']}] | Step : [{batch_idx+1}/{len(train_loader)}]| Loss : {loss.item()}")
-            
-            training_loss_per_epoch.append(np.min(train_loss_scores))
+                if (batch_idx+1) % 20 == 0:
+                    print(f"Epoch : [{epoch+1}/{TRAINING_CONFIG['NUM_EPOCHS']}] | Step : [{batch_idx+1}/{len(train_loader)}]|  Average Training Loss : {np.mean(train_loss_scores)}")
+    
+            training_loss_per_epoch.append(np.mean(train_loss_scores))
             
             model.eval()
             with torch.no_grad():
@@ -54,7 +54,11 @@ def network_training(model, criterion, optimizer, train_loader, validation_loade
                     loss = criterion(target, predictions)
                     validation_loss_scores.append(loss.item())
                     
-            validation_loss_per_epoch.append(np.min(validation_loss_scores))
+                    if (batch_idx+1) % 20 == 0:
+                        print(f"Epoch : [{epoch+1}/{TRAINING_CONFIG['NUM_EPOCHS']}] | Step : [{batch_idx+1}/{len(validation_loader)}]|  Average Validation Loss : {np.mean(validation_loss_scores)}")
+
+                    
+            validation_loss_per_epoch.append(np.mean(validation_loss_scores))
             
             end_training_time = datetime.datetime.now()
             print("==================================================================================================================================================")
@@ -67,7 +71,7 @@ def network_training(model, criterion, optimizer, train_loader, validation_loade
                 best_loss = validation_loss_per_epoch[-1]
                 idle_training_epochs = 0
                 time = datetime.datetime.now().date()
-                model.save_model(filename=f"{time}")  
+                model.save_model(filename=f"{time}_best_loss_{best_loss}")  
             else:
                 pass
             
